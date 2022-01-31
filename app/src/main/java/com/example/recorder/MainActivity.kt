ac2d0dadbc3bbdbcaff7f903165fe004aca137bf
hwayeon <hwayeon351@gmail.com>
2022-01-31 14:18:49 +0900

    feat: Update layout and add RECORD_AUDIO permission

--- app/src/main/java/com/example/recorder/MainActivity.kt
+++ app/src/main/java/com/example/recorder/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.recorder
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy{
+        findViewById(R.id.recordButton)
+    }
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private var state = State.BEFORE_RECORDING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestAudioPermission()
+        initView()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initView() {
+        recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
